398d964e94cfd55c6d88da59deb7cc1ed7c4fc1b
AlexandruArh <151926391+AlexandruArh@users.noreply.github.com>
2026-07-19 11:45:54 +0100

    Add Vercel deployment config: vercel.json, build_files.sh, wsgi entry

--- fitconnect/wsgi.py
+++ fitconnect/wsgi.py
@@ -1,7 +1,7 @@
-"""WSGI config for FitConnect - also used as Vercel entrypoint."""
+"""WSGI config for fitconnect project."""
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitconnect.settings')
-app = get_wsgi_application()  # Vercel looks for `app`
-application = app  # gunicorn / standard WSGI
+
+app = get_wsgi_application()
