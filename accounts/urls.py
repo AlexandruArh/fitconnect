948a2b6d65e3cf1b39e5ed7356ecba118d12cd40
AlexandruArh <151926391+AlexandruArh@users.noreply.github.com>
2026-07-25 17:49:24 +0100

    Add new account management routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path("personal-details/", views.personal_details_view, name="personal_details"),
+    path("change-password/", views.change_password_view, name="change_password"),
+    path("delete-account/", views.delete_account_view, name="delete_account"),
 ]
