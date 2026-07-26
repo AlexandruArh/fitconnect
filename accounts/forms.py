a8fa01abff2fb0b5716ab3d2fbf00abcf5d1f4f6
AlexandruArh <151926391+AlexandruArh@users.noreply.github.com>
2026-07-26 10:22:04 +0100

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -43,3 +43,14 @@ class RegisterForm(forms.Form):
             password=password
         )
         return user
+
+
+class PersonalDetailsForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
+        }
